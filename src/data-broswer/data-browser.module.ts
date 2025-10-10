222a245c39c26ef71cfdf8a923f01c1316913b51
bgw4399 <104902657+bgw4399@users.noreply.github.com>
2025-10-10 18:18:07 +0900

    모듈 오류 수정

--- src/data-broswer/data-browser.module.ts
+++ src/data-broswer/data-browser.module.ts
@@ -1,11 +1,17 @@
-// src/data-browser/data-browser.module.ts
-
 import { Module } from '@nestjs/common';
 import { DataBrowserController } from './data-browser.controller';
 import { DataBrowserService } from './data-browser.service';
+import { Kysely } from 'kysely';
+import { db } from '../db/types';   
 
 @Module({
   controllers: [DataBrowserController],
-  providers: [DataBrowserService],
+  providers: [
+    DataBrowserService,
+    {
+      provide: Kysely,
+      useValue: db,
+    },
+  ],
 })
 export class DataBrowserModule {}
